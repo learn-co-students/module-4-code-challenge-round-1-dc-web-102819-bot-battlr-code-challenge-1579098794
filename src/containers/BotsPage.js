fd6df3d477d7180ecd86d973d39514566fca468f
mattbechtel1 <bechma02@gmail.com>
2020-01-15 09:48:23 -0500

    step-2-complete

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,31 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import BotArmy from './YourBotArmy'
+
+const botUrl = 'https://bot-battler-api.herokuapp.com/api/v1/bots'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor() {
+    super()
+    this.state = {
+      allBots: [],
+      myArmy: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(botUrl)
+    .then(response => response.json())
+    .then(data => this.setState({allBots: data}))
+  }
+
+  filteredBots = () => this.state.allBots.filter(bot => !this.state.myArmy.includes(bot))
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <BotCollection bots={this.filteredBots()} />
       </div>
     );
   }
